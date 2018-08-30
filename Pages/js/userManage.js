dcb2d776b4f19a348bb9c5e21d1057a2d7933d59
LucienTao <misakayu@163.com>
2018-08-30 21:16:35 +0800

    finishUserManager

--- Pages/js/userManage.js
+++ Pages/js/userManage.js
@@ -4,22 +4,113 @@ new Vue({
         return {
             visible: false,
             remember: false,
-            isCollapse: true,
+            isCollapse: false,
             userName: "DefaultUser",
-            sideMenuState: "收起"
+            sideMenuState: "收起",
+            headImgUrl: "https://www.setaswall.com/wp-content/uploads/2017/10/3d-Afloat-Drop-Img-Wallpaper-1080x1920.jpg",
+            newUserName: "",
+            newUserPhone: "",
+            tableData: [{
+                id: 1,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }, {
+                id: 2,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }, {
+                id: 3,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }, {
+                id: 4,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }, {
+                id: 5,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }, {
+                id: 6,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }, {
+                id: 7,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }, {
+                id: 8,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }, {
+                id: 9,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }, {
+                id: 10,
+                name: "user",
+                phone: "123",
+                date: "2018-09-30"
+            }],
+            currentPage: 1,
+            pageSize: 10,
+            totalNum: 100,
         }
     },
     methods: {
-        handleOpen(key, keyPath) {
-          console.log(key, keyPath);
+        handleMenuSelect(key, keyPath) {
+            console.log(key, keyPath);
         },
-        handleClose(key, keyPath) {
-          console.log(key, keyPath);
-        },
-        handleSideMenuCollapse(){
+        handleSideMenuCollapse() {
             this.sideMenuState = this.isCollapse == true ? "展开" : "收起";
             this.isCollapse = !this.isCollapse;
-            
+        },
+        handleCreateUser() {
+            console.log("newUser");
+        },
+        handleEdit(index, row) {
+            console.log(index, row);
+        },
+        handleDelete(index, row) {
+            console.log(index, row);
+        },
+        handleCurrentChange(val) {
+            console.log(`当前页: ${val}`);
+        },
+        handlePrevPage() {
+
+        },
+        handleNextPage() {
+
         }
-      }
+    },
+    mounted() {
+        // axios.get('/user?ID=12345')
+        //     .then(function (response) {
+        //         console.log(response);
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
+        // axios.post('/user', {
+        //         firstName: 'Fred',
+        //         lastName: 'Flintstone'
+        //     })
+        //     .then(function (response) {
+        //         console.log(response);
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
+    },
+
 })
